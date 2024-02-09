c1d09115cb803973fbdd40d8f97b0983170c2a42
trunkatree <26478409+trunkatree@users.noreply.github.com>
2024-02-09 22:39:28 +0900

    add comments

--- cmd/niwa/main.go
+++ cmd/niwa/main.go
@@ -6,10 +6,15 @@ import (
 
 var shadow bool
 
+// 関数やメソッド、構造体などの型の定義の前に空行なしでコメントを書くと、ドキュメントとして利用される
 func main() {
 	n, _ := fmt.Println("Hello, world!", "...and", "Hello, world!")
 	fmt.Println(n, "bytes written.")
 
+	/*
+	 * シャドーイング
+	 * コードブロックの中で宣言された変数は、コードブロックの外の同名の変数に影響をおよぼさない
+	 */
 	shadow = true
 	// shadow = false
 	x := 0
