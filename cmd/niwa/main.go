7cc902ea7a23024ba5ceb3f3f008d8e488cbaba2
trunkatree <26478409+trunkatree@users.noreply.github.com>
2024-02-11 18:06:19 +0900

    add type converstion samples

--- cmd/niwa/main.go
+++ cmd/niwa/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 var shadow bool
@@ -26,4 +27,19 @@ func main() {
 		fmt.Println("inside of if: x =", x)
 	}
 	fmt.Println("outside of if: x =", x)
+
+	var i int = 123
+	var i64 int64
+	i64 = int64(i)      // 明示的な型変換が必要
+	var b bool = i != 0 // boolへの変換には比較演算子を使う
+	fmt.Println(i, i64, b)
+
+	var istr string
+	var iint int64
+	istr = strconv.FormatInt(i64, 10)           // 文字列への変換はFormat系関数
+	iint, err := strconv.ParseInt(istr, 10, 64) // 文字列からの変換はParse系関数
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(istr, iint)
 }
